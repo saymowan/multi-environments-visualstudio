a17de77cb84c365e3128e8f9fc3972859568e34c
Saymon <saymon.oliveira@base2.com.br>
2019-03-28 11:21:55 -0300

    Arquivos do projeto

---
+++ ConfigProject/ConfigProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Configuration;
+
+namespace ConfigProject
+{
+
+    public class UnitTest1
+    {
+        [Test]
+        public void ValidarAmbientes()
+        {
+
+            Console.WriteLine("Nome Branch: " + ConfigurationManager.AppSettings["nome_branch"].ToString());
+            Console.WriteLine("Database: " + ConfigurationManager.AppSettings["database"].ToString());
+
+        }
+    }
+}
